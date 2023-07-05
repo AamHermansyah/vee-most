f63453eca27a1b7fb74a41e98ccfd78add40a326
pandupan <pangestup60@gmail.com>
2023-07-05 08:35:53 +0700

    cart page done

--- app/cart/page.js
+++ app/cart/page.js
@@ -7,12 +7,25 @@ import Typography from "@mui/material/Typography";
 import Stack from "@mui/material/Stack";
 import NavigateNextIcon from "@mui/icons-material/NavigateNext";
 import Checkbox from "@mui/material/Checkbox";
-import Button from "@mui/material/Button";
 import productPicture from "../../assets/vee-store-product.png";
 import trash from "../../assets/delete.png"
 import save from "../../assets/floppy-disk.png"
+import TextField from '@mui/material/TextField';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import PayPalPayment from '@/components/PayPalCheckout';
+import { Button } from "@mui/material";
+
 
 function Checkout() {
+  const [age, setAge] = React.useState('');
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+
   const [number, setNumber] = React.useState(1);
 
   const updateQuantity = (id, value) => {
@@ -41,25 +54,13 @@ function Checkout() {
                     {breadcrumbs}
                   </Breadcrumbs>
                 </Stack>
-                <Button
-                  variant="outlined"
-                  sx={{
-                    fontSize: "0.75rem",
-                    padding: "4px 8px",
-                    borderRadius: "xl",
-                    borderColor: "black",
-                    color: "black",
-                  }}
-                >
-                  Quick Order
-                </Button>
               </div>
               
               <div className="flex gap-2 mt-4">
                 <Typography className="font-bold text-gray-700 text-2xl">My Cart</Typography>
               </div>  
             </div>
-            <div className="flex justify-between">
+            <div className="flex justify-between sm:flex-col md:flex-col lg:flex-row">
             <div className="max-w-[1000px] mx-auto relative overflow-x-auto basis-[70%]">
               <table className="min-w-[900px] w-full text-sm text-left text-gray-500">
                 <thead className="text-xs text-gray-700 uppercase">
@@ -147,8 +148,200 @@ function Checkout() {
                 <div className="h-1 bg-yellow-400" />
               </div>
             </div>
-            <div className="basis-[30%] w-full h-full bg-red-700">
-                  
+            <div className="basis-[30%] w-full h-auto mx-8 lg:-mt-16 sm:mt-0 md:mt-0 ">
+              <div className="flex flex-col">
+                <div className="text-md font-semibold">
+                  <h1 className="mt-2 ml-3 mb-2">Summary</h1>
+                  <div className="inset-0">
+                    <div className="h-[2px] w-auto bg-gray-200" />
+                  </div>
+                </div>
+                <div className="text-md font-medium">
+                  <h2 className="mt-2 ml-3 mb-2">Estimate Shipping and Tax</h2>
+                </div>
+                <div className="mb-2 mx-4">
+                    <h3 className="font-300">
+                      Country 
+                    </h3>
+                    <FormControl fullWidth className="w-full">
+                      <InputLabel id="demo-simple-select-label">United States</InputLabel>
+                      <Select
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                        placeholderl="Country"
+                        onChange={handleChange}
+                      >
+                        <MenuItem value={10}>Ten</MenuItem>
+                        <MenuItem value={20}>Twenty</MenuItem>
+                        <MenuItem value={30}>Thirty</MenuItem>
+                      </Select>
+                    </FormControl>
+                  </div>
+                <div className="mb-2 mx-4">
+                    <h3 className="font-300">
+                      State/Province
+                    </h3>
+                    <FormControl fullWidth className="w-full">
+                      <InputLabel id="demo-simple-select-label">Ohio</InputLabel>
+                      <Select
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                        placeholderl="Country"
+                        onChange={handleChange}
+                      >
+                        <MenuItem value={10}>Ten</MenuItem>
+                        <MenuItem value={20}>Twenty</MenuItem>
+                        <MenuItem value={30}>Thirty</MenuItem>
+                      </Select>
+                    </FormControl>
+                  </div>
+                  <div className="mb-2 mx-4">
+                    <h3 className="font-300">
+                      Zip/Postal Code
+                    </h3>
+                    <TextField className="w-full" id="outlined-basic"  placeholder="44311" variant="outlined" />
+                  </div>
+                  <div className="mb-4 mx-4 mt-4">
+                    <h3 className="font-300">
+                      Shipped by Your Account
+                    </h3>
+                    <div className="flex">
+                      <input
+                        id="default-radio-1"
+                        type="radio"
+                        defaultValue=""
+                        name="default-radio"
+                        className="w-[15px] sm:w-[20px] aspect-square text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500"
+                      />
+                      <label
+                        htmlFor="default-radio-1"
+                        className="text-xs sm:text-base ml-4 flex items-center gap-4 font-normal text-gray-900"
+                      >
+                        Your Account
+                        <p className="font-bold -ml-2 text-sm">
+                        $15,966.90
+                        </p>
+                      </label>
+                    </div>
+                  </div>
+                  <div className="mb-2 mx-4">
+                    <h3 className="font-300">
+                      Table Rate
+                    </h3>
+                    <div className="flex">
+                      <input
+                        id="default-radio-1"
+                        type="radio"
+                        defaultValue=""
+                        name="default-radio"
+                        className="w-[15px] sm:w-[20px] aspect-square text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500"
+                      />
+                      <label
+                        htmlFor="default-radio-1"
+                        className="text-xs sm:text-base ml-4 flex items-center gap-4 font-normal text-gray-900"
+                      >
+                        UPS WOrldwide Expedited 
+                        <p className="font-bold -ml-2 text-sm">
+                        $26.00
+                        </p>
+                      </label>
+                    </div>
+                    <div className="flex">
+                      <input
+                        id="default-radio-1"
+                        type="radio"
+                        defaultValue=""
+                        name="default-radio"
+                        className="w-[15px] sm:w-[20px] aspect-square text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500"
+                      />
+                      <label
+                        htmlFor="default-radio-1"
+                        className="text-xs sm:text-base ml-4 flex items-center gap-4 font-normal text-gray-900"
+                      >
+                        UPS Worldwide Express Saver
+                        <p className="font-bold -ml-2 text-sm">
+                        $29.00
+                        </p>
+                      </label>
+                    </div>
+                    <div className="flex">
+                      <input
+                        id="default-radio-1"
+                        type="radio"
+                        defaultValue=""
+                        name="default-radio"
+                        className="w-[15px] sm:w-[20px] aspect-square text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500"
+                      />
+                      <label
+                        htmlFor="default-radio-1"
+                        className="text-xs sm:text-base ml-4 flex items-center gap-4 font-normal text-gray-900"
+                      >
+                        DHL Priority
+                        <p className="font-bold -ml-2 text-sm">
+                        $33.00
+                        </p>
+                      </label>
+                    </div>
+                    <div className="flex">
+                      <input
+                        id="default-radio-1"
+                        type="radio"
+                        defaultValue=""
+                        name="default-radio"
+                        className="w-[15px] sm:w-[20px] aspect-square text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500"
+                      />
+                      <label
+                        htmlFor="default-radio-1"
+                        className="text-xs sm:text-base ml-4 flex items-center gap-4 font-normal text-gray-900"
+                      >
+                        FedEx Priority
+                        <p className="font-bold -ml-2 text-sm">
+                        $35.00
+                        </p>
+                      </label>
+                    </div>
+                    <div className="flex">
+                      <input
+                        id="default-radio-1"
+                        type="radio"
+                        defaultValue=""
+                        name="default-radio"
+                        className="w-[15px] sm:w-[20px] aspect-square text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500"
+                      />
+                      <label
+                        htmlFor="default-radio-1"
+                        className="text-xs sm:text-base ml-4 flex items-center gap-4 font-normal text-gray-900"
+                      >
+                        EMS Speed Post
+                        <p className="font-bold -ml-2 text-sm">
+                        $51.00
+                        </p>
+                      </label>
+                    </div>
+                  </div>
+                  <div className="flex justify-between text-md font-normal">
+                    <p className="mt-2 ml-3 mb-2">Subtotal</p>
+                    <p className="mt-2 ml-3 mb-2">U$221.00</p>
+                  </div>
+                  <div className="flex justify-end text-md font-normal">
+                    <p className="mt-2 ml-3 mb-2">Shipping (Table Rate - UPS Worldwide Expedited) "</p>
+                    <p className="mt-2 ml-3 mb-2">U$221.00</p>
+                    <div className="inset-0">
+                      <div className="h-[2px] w-auto bg-gray-200" />
+                    </div>
+                  </div>
+                  <div className="inset-0">
+                    <div className="h-[2px] w-auto bg-gray-200" />
+                  </div>
+                  <div className="text-md font-semibold flex justify-between ">
+                    <h1 className="mt-2 ml-3 mb-2 text-gray-500">Order Total</h1>
+                    <h1 className="mt-2 ml-3 mb-2 text-red-400">US$247.00</h1>
+                  </div>
+                  <Button variant="contained" color="primary">
+                    Proceed to Checkout
+                  </Button>
+                  <PayPalPayment className="-mt-8" />
+              </div>
             </div>
             </div>
           </div>
